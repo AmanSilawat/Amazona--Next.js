148501dfd026f368f34fe833c72cb683bf57f307
amansilawat <aman.khan.silawat.aks@gmail.com>
2021-08-20 14:57:47 +0530

    Route product link

--- components/Layout.js
+++ components/Layout.js
@@ -1,29 +1,39 @@
-import React from 'react'
+
+import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core';
+import useStyles from '../utils/styles';
+import NextLink from 'next/link';
 
-
-const Layout = ({ children }) => {
+export default function Layout({ children }) {
+    const classes = useStyles();
     return (
         <div>
             <Head>
                 <title>Amazona Next</title>
             </Head>
-            <AppBar position="static">
+            <AppBar position="static" className={classes.navbar}>
                 <Toolbar>
-                    <Typography>Amazona Next</Typography>
+                    <NextLink href="/" passHref>
+                        <Link>
+                            <Typography className={classes.brand}>amazona</Typography>
+                        </Link>
+                    </NextLink>
+                    <div className={classes.grow}></div>
+                    <div>
+                        <NextLink href="/cart" passHref>
+                            <Link>Cart</Link>
+                        </NextLink>
+                        <NextLink href="/login" passHref>
+                            <Link>Login</Link>
+                        </NextLink>
+                    </div>
                 </Toolbar>
             </AppBar>
-            <Container>
-                {children}
-            </Container>
-            <footer>
-                <Typography>
-                    All reserve save in Amazona Next
-                </Typography>
+            <Container className={classes.main}>{children}</Container>
+            <footer className={classes.footer}>
+                <Typography>All rights reserved. Amazona Next.</Typography>
             </footer>
         </div>
-    )
+    );
 }
-
-export default Layout
